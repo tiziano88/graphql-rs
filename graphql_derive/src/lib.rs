aa549d972d15b4d2b620e62d19514839e4251f0c
Tiziano Santoro <tiziano88@gmail.com>
2018-02-23 17:37:37 +0000

    Depend on serde

--- graphql_derive/src/lib.rs
+++ graphql_derive/src/lib.rs
@@ -1,9 +1,12 @@
 extern crate proc_macro;
 #[macro_use]
 extern crate quote;
+extern crate serde_derive_internals;
 extern crate syn;
 
 use proc_macro::TokenStream;
+use serde_derive_internals::attr::Container;
+use serde_derive_internals::Ctxt;
 
 trait GraphQL {
     fn query() -> String;
@@ -26,7 +29,9 @@ fn impl_graph_ql(ast: &syn::DeriveInput) -> quote::Tokens {
     let name = &ast.ident;
     if let syn::Body::Struct(ref variants) = ast.body {
         for f in variants.fields() {
-            let field_name = if let Some(rename) = f.attrs.iter().find(|a| a.name() == "rename") {
+            Container::from_ast(&Ctxt::new(), ast);
+            let field_name = if let Some(rename) = f.attrs.iter().find(|a| a.name() == "serde") {
+                q = format!("{}--{:?}--\n", q, rename.name());
                 match rename.value {
                     syn::MetaItem::NameValue(_, syn::Lit::Str(ref v, _)) => v.clone().to_string(),
                     _ => "xxx".to_string(),
